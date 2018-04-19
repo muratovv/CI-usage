98c5d5cc0cd1772b58d7298c10e91828f99e592e
Fedor Muratov <muratovfyodor@yandex.ru>
2018-04-19 14:46:53 +0300

    fix build

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ fi
 
 rm -rf build
 mkdir build && cd build
-conan install .. -s build_type=$BUILD_TYPE
+conan install .. -s build_type=$BUILD_TYPE --build=missing
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER
 cmake --build . -- VERBOSE=1
 ctest -V
